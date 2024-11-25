5135e0a34c8bd43c7a143916e861364109ee9cec
Павлова Дарья <pavlova7346@gmail.com>
2024-11-25 17:53:16 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,56 +1,79 @@
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const names = [
+  'Мария',
+  'Алина',
+  'Алёна',
+  'Дарья',
+  'Кристина',
+  'Светлана',
+  'Вячеслав',
+  'Анастасия'
+];
 
-const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
+const descriptions = [
+  'Закат',
+  'Осень',
+  'Вечер',
+  'Утро',
+  'Завтрак',
+  'Природа',
+  'Работа',
+  'Спорт',
+  'Книги',
+  'Планы',
+];
 
-const generateComments = (count) => {
-  const messages = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  const names = [
-    'Мария',
-    'Алина',
-    'Алёна',
-    'Дарья',
-    'Кристина',
-    'Светлана',
-    'Вячеслав',
-    'Анастасия'
-  ];
-  return Array.from({ length: count }, () => ({
-    id: getRandomNumber(1, 1000),
-    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-    message: Array.from({ length: getRandomNumber(1, 2) }, () => getRandomElement(messages)).join(' '),
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function createComment() {
+  return {
+    id: getRandomNum(1, 1000),
+    avatar: `img/avatar-${getRandomNum(1, 6)}.svg`,
+    message: getRandomElement(messages),
     name: getRandomElement(names)
-  }));
-};
+  };
+}
 
-const generatePhotos = () => {
-  const descriptions = [
-    'Закат',
-    'Осень',
-    'Вечер',
-    'Утро',
-    'Завтрак',
-    'Природа',
-    'Работа',
-    'Спорт',
-    'Книги',
-    'Планы',
-  ];
+function createComments() {
+  const comments = [];
+  const count = getRandomNum(0, 30);
 
-  return Array.from({ length: 25 }, (_, i) => ({
-    id: i + 1,
-    url: `photos/${i + 1}.jpg`,
-    description: getRandomElement(descriptions),
-    likes: getRandomNumber(15, 200),
-    comments: generateComments(getRandomNumber(0, 30))
-  }));
-};
+  for (let i = 0; i < count; i++) {
+    comments.push(createComment());
+  }
 
-// eslint-disable-next-line no-console
-console.log(generatePhotos());
+  return comments;
+}
+
+function generateObjects() {
+  const photos = [];
+
+  for (let i = 1; i <= 25; i++) {
+    const photo = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomElement(descriptions),
+      likes: getRandomNum(15, 200),
+      comments: createComments()
+    };
+
+    photos.push(photo);
+  }
+
+  return photos;
+}
+
+generateObjects();
