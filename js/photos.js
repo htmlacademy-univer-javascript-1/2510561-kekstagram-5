c7d6b3975eecd01517d49105b7ab140c084923c6
Павлова Дарья <pavlova7346@gmail.com>
2024-12-07 01:18:02 +0500

    Отрисуй меня полностью

--- js/photos.js
+++ js/photos.js
@@ -1,6 +1,6 @@
-import {createArrayOfPhotos} from "./data.js";
-const picturesContainer = document.querySelector(".pictures");
-const pictureTemplate = document.querySelector("#picture").content.querySelector(".picture");
+import {createArrayOfPhotos} from './data.js';
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureFragment = document.createDocumentFragment();
 
 const photos = createArrayOfPhotos();
@@ -8,9 +8,9 @@ const photos = createArrayOfPhotos();
 photos.forEach((photo) => {
   const picture = pictureTemplate.cloneNode(true);
 
-  picture.querySelector(".picture__img").src = photo.url;
-  picture.querySelector(".picture__comments").textContent = photo.comments.length;
-  picture.querySelector(".picture__likes").textContent = photo.likes;
+  picture.querySelector('.picture__img').src = photo.url;
+  picture.querySelector('.picture__comments').textContent = photo.comments.length;
+  picture.querySelector('.picture__likes').textContent = photo.likes;
 
   pictureFragment.append(picture);
 });
