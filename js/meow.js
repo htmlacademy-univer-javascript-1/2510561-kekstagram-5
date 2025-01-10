f3eb6b4a723d9b51f48dca7715deb1945561e8ec
Павлова Дарья <pavlova7346@gmail.com>
2025-01-11 00:21:59 +0500

    правки123

--- js/meow.js
+++
@@ -1,7 +0,0 @@
-export function getRandomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export function getRandomElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
---
+++ js/rendering-full-size-image.js
@@ -0,0 +1,25 @@
+import { renderThumbnails } from './displaying-thumbnails.js';
+import { showFullSizePicture } from './displaying-gallery.js';
+
+const pictureContainerElement = document.querySelector('.pictures');
+let pictures = [];
+
+const onPictureContainerElementClick = (evt) => {
+  const thumbnail = evt.target.closest('[data-thumbnail-id]');
+
+  if (!thumbnail) {
+    return;
+  }
+  evt.preventDefault();
+
+  const picture = pictures.find((item) =>
+    item.id === parseInt(thumbnail.dataset.thumbnailId, 10)
+  );
+  showFullSizePicture(picture);
+};
+
+export const renderGallery = (currentPictures) => {
+  pictures = currentPictures;
+  renderThumbnails(pictures);
+  pictureContainerElement.addEventListener('click', onPictureContainerElementClick);
+};
